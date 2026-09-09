8ff11d71c16f965603e25c429c7f27ff71e993c8
shubham-mamodiya-dev <shubham.mamodiya.dev@gmail.com>
2026-09-09 21:06:51 +0530

    added default implementation, keeping everyting empty by default.

--- src/graph.rs
+++ src/graph.rs
@@ -1,4 +1,14 @@
+#[derive(Debug, Default)]
 pub struct Graph<T> {
     adj: Vec<Vec<usize>>,
     values: Vec<T>,
+}
+
+impl<T> Graph<T> {
+    pub fn new() -> Self {
+        Self {
+            adj: Vec::new(),
+            values: Vec::new(),
+        }
+    }
 }
