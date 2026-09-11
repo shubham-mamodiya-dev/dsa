347f15dea701c160270fdc31a72ba32822f27046
shubham-mamodiya-dev <shubham.mamodiya.dev@gmail.com>
2026-09-11 21:47:55 +0530

    added add_edge method and made add_vertex only add vertex if it don't yet exists.

--- src/graph.rs
+++ src/graph.rs
@@ -7,7 +7,7 @@ pub struct Graph<T> {
     index: HashMap<T, usize>,
 }
 
-impl<T: Eq + Hash> Graph<T> {
+impl<T: Eq + Hash + Clone> Graph<T> {
     pub fn new() -> Self {
         Self {
             adj: Vec::new(),
@@ -15,11 +15,35 @@ impl<T: Eq + Hash> Graph<T> {
         }
     }
 
-    pub fn add_edge(&self, x: T, y: T) {}
+    /// Adds edge between x and y. If they don't exist then it creates them.
+    ///
+    /// * `x`: vertex
+    /// * `y`: vertex
+    pub fn add_edge(&mut self, x: T, y: T) {
+        if x == y {
+            return;
+        }
+        if !(self.index.contains_key(&x)) {
+            self.add_vertex(x.clone());
+        }
+        if !(self.index.contains_key(&y)) {
+            self.add_vertex(y.clone());
+        }
 
+        if let (Some(index_x), Some(index_y)) = (self.index.get(&x), self.index.get(&y)) {
+            self.adj[*index_x].push(*index_y);
+            self.adj[*index_y].push(*index_x);
+        };
+    }
+
+    /// Adds vertes in the graph only if it don't exist yet.
+    ///
+    /// * `v`: The vertex
     pub fn add_vertex(&mut self, v: T) {
         let index = self.adj.len();
-        self.adj.push(Vec::new());
-        self.index.insert(v, index);
+        if !self.index.contains_key(&v) {
+            self.adj.push(Vec::new());
+        }
+        self.index.entry(v).or_insert(index);
     }
 }
