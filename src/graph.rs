c5b7a7fe4ae25f48ba7ebd0e978eccfef35d7c76
shubham-mamodiya-dev <shubham.mamodiya.dev@gmail.com>
2026-09-11 21:22:28 +0530

    added functionality for adding vertexes.

--- src/graph.rs
+++ src/graph.rs
@@ -1,20 +1,25 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 
 #[derive(Debug, Default)]
 pub struct Graph<T> {
     adj: Vec<Vec<usize>>,
-    values: Vec<T>,
     index: HashMap<T, usize>,
 }
 
-impl<T> Graph<T> {
+impl<T: Eq + Hash> Graph<T> {
     pub fn new() -> Self {
         Self {
             adj: Vec::new(),
-            values: Vec::new(),
             index: HashMap::new(),
         }
     }
 
     pub fn add_edge(&self, x: T, y: T) {}
+
+    pub fn add_vertex(&mut self, v: T) {
+        let index = self.adj.len();
+        self.adj.push(Vec::new());
+        self.index.insert(v, index);
+    }
 }
