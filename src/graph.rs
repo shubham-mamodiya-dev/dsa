36f6b9ee15df9dd6c50c96fe410036bf24a0fb92
shubham-mamodiya-dev <shubham.mamodiya.dev@gmail.com>
2026-09-12 15:16:27 +0530

    added some useful methods to graph api

--- src/graph.rs
+++ src/graph.rs
@@ -8,7 +8,7 @@ pub struct Graph<T> {
     values: Vec<T>,
 }
 
-impl<T: Eq + Hash + Clone> Graph<T> {
+impl<T: Eq + Hash + Clone + PartialEq> Graph<T> {
     pub fn new() -> Self {
         Self {
             adj: Vec::new(),
@@ -80,11 +80,56 @@ impl<T: Eq + Hash + Clone> Graph<T> {
     }
 
     /// Returns number of vertices in the graph
-    pub fn count(&self) -> usize {
+    pub fn count_vertices(&self) -> usize {
         self.values.len()
     }
 
     pub fn count_edges(&self) -> usize {
         self.adj.iter().map(|x| x.len()).sum::<usize>() / 2
+    }
+
+    pub fn vertices(&self) -> Vec<T> {
+        self.values.clone()
+    }
+
+    pub fn is_edge(&self, x: &T, y: &T) -> bool {
+        if !self.contains(x) || !self.contains(y) {
+            return false;
+        }
+
+        if x == y {
+            return true;
+        }
+
+        let x_index = self.index.get(x);
+        let y_index = self.index.get(y);
+
+        if let (Some(&x_index), Some(y_index)) = (x_index, y_index) {
+            self.adj[x_index].contains(y_index)
+        } else {
+            false
+        }
+    }
+
+    /// Returns number or vertices connected with v. Returns None if vertex v
+    /// doesn't exists in the graph.
+    ///
+    /// * `v`: Vertex
+    pub fn degree(&self, v: &T) -> Option<usize> {
+        self.index.get(v).map(|&x| self.adj[x].len())
+    }
+
+    pub fn max_degree(&self) -> usize {
+        self.values
+            .iter()
+            // Unwrap is fine here. Because we are computing degree of vertices
+            // of degrees that exists in the graph.
+            .map(|x| self.degree(x).unwrap())
+            .max()
+            .unwrap_or(0)
+    }
+
+    pub fn average_degree(&self) -> f64 {
+        2.0 * self.count_edges() as f64 / self.count_vertices() as f64
     }
 }
