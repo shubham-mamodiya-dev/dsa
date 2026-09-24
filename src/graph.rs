63e8d0cdbb93c4fe28cdc9d65186459e8a0e0f1a
shubham-mamodiya-dev <shubham.mamodiya.dev@gmail.com>
2026-09-24 11:15:05 +0530

    Adding a saperate Connected Component type rather then using the already present graph or diagraph internally.

--- src/graph.rs
+++ src/graph.rs
@@ -1,6 +1,5 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::hash::Hash;
-use std::iter;
 
 #[derive(Debug, Default)]
 pub struct Graph<T> {
@@ -191,6 +190,8 @@ impl<T: Eq + Hash + Clone + PartialEq> Graph<T> {
             if !marked[adj] {
                 self.dfs(adj, w, marked, edge_to);
                 edge_to[adj] = v;
+
+                // only keep crawling if destination is not found.
                 if adj == w {
                     return;
                 }
@@ -469,4 +470,252 @@ impl<T: Eq + Clone + PartialEq + Hash> Digraph<T> {
         }
         // If they are connected then from destination_vertex trace the starting_vertex
     }
+}
+
+#[derive(Debug, Default)]
+pub struct CC<T> {
+    adj: Vec<HashSet<usize>>,
+    index: HashMap<T, usize>,
+    values: Vec<T>,
+}
+
+impl<T: Eq + Hash + Clone + PartialEq> CC<T> {
+    pub fn new() -> Self {
+        Self {
+            adj: Vec::new(),
+            index: HashMap::new(),
+            values: Vec::new(),
+        }
+    }
+
+    /// Adds edge between x and y. If they don't exist then it creates them.
+    ///
+    /// * `x`: vertex
+    /// * `y`: vertex
+    pub fn add_edge(&mut self, x: T, y: T) {
+        if x == y {
+            return;
+        }
+
+        // add_vertex checks only add vertex if it doesn't exist. But, for new
+        // lets keep validation here just for explicitness
+        if !(self.contains(&x)) {
+            self.add_vertex(x.clone());
+        }
+        if !(self.contains(&y)) {
+            self.add_vertex(y.clone());
+        }
+
+        // We can assume x and y exists as vertices.
+        if let (Some(&x), Some(&y)) = (self.index.get(&x), self.index.get(&y)) {
+            self.adj[x].insert(y);
+            self.adj[y].insert(x);
+        };
+    }
+
+    /// Adds vertex in the graph only if it doesn't exist yet.
+    ///
+    /// * `v`: The vertex
+    pub fn add_vertex(&mut self, v: T) {
+        if self.contains(&v) {
+            return;
+        }
+
+        // only add vertex if it doesn't exist
+        let index = self.adj.len();
+        self.values.push(v.clone());
+        self.adj.push(HashSet::new());
+        self.index.insert(v, index);
+    }
+
+    pub fn adjacent_vertices(&self, v: &T) -> impl Iterator<Item = &T> {
+        self.get_index(v)
+            .into_iter()
+            // Above iterator only returns one element the index
+            .flat_map(move |&x| self.adj[x].iter())
+            .map(|&x| &self.values[x])
+    }
+
+    pub(crate) fn adjacent_vertex_indices(&self, v: &T) -> impl Iterator<Item = &usize> {
+        self.get_index(v)
+            .into_iter()
+            // Above iterator only returns one element the index
+            .flat_map(move |&x| self.adj[x].iter())
+    }
+
+    pub(crate) fn value(&self, i: usize) -> T {
+        self.values[i].clone()
+    }
+
+    pub(crate) fn adjacent_vertex_indices_from_index(
+        &self,
+        i: usize,
+    ) -> impl Iterator<Item = &usize> {
+        self.adj[i].iter()
+    }
+    /// Checks if Vertex exists in the graph
+    ///
+    /// * `v`: Vertex
+    pub fn contains(&self, v: &T) -> bool {
+        self.index.contains_key(v)
+    }
+
+    /// Returns number of vertices in the graph
+    pub fn count_vertices(&self) -> usize {
+        self.values.len()
+    }
+
+    pub fn count_edges(&self) -> usize {
+        self.adj.iter().map(|x| x.len()).sum::<usize>() / 2
+    }
+
+    pub(crate) fn get_index(&self, v: &T) -> Option<&usize> {
+        self.index.get(v)
+    }
+
+    pub fn vertices(&self) -> impl Iterator<Item = &T> {
+        self.values.iter()
+    }
+
+    pub fn is_edge(&self, x: &T, y: &T) -> bool {
+        if !self.contains(x) || !self.contains(y) {
+            return false;
+        }
+
+        if x == y {
+            return true;
+        }
+
+        let x_index = self.get_index(x);
+        let y_index = self.get_index(y);
+
+        if let (Some(&x_index), Some(y_index)) = (x_index, y_index) {
+            self.adj[x_index].contains(y_index)
+        } else {
+            false
+        }
+    }
+
+    /// Returns number or vertices connected with v. Returns None if vertex v
+    /// doesn't exists in the graph.
+    ///
+    /// * `v`: Vertex
+    pub fn degree(&self, v: &T) -> Option<usize> {
+        self.get_index(v).map(|&x| self.adj[x].len())
+    }
+
+    pub fn max_degree(&self) -> usize {
+        self.values
+            .iter()
+            // Unwrap is fine here. Because we are computing degree of vertices
+            // of degrees that exists in the graph.
+            .map(|x| self.degree(x).unwrap())
+            .max()
+            .unwrap_or(0)
+    }
+
+    pub fn average_degree(&self) -> f64 {
+        2.0 * self.count_edges() as f64 / self.count_vertices() as f64
+    }
+
+    // pub fn path_with_dfs(&self, v: &T, w: &T) -> impl Iterator<Item = T> {
+    //     // Making sure v and w exists in the graph
+    //     if self.get_index(v).is_none() || self.get_index(w).is_none() {
+    //         return Vec::new().into_iter();
+    //     }
+    //
+    //     // Finding a path between.
+    //
+    //     let mut edge_to: Vec<usize> = (0..self.count_vertices()).collect();
+    //     let mut marked = vec![false; self.count_vertices()];
+    //     let &starting_vertex = self.get_index(v).unwrap();
+    //     let &destination_vertex = self.get_index(w).unwrap();
+    //
+    //     self.dfs(
+    //         starting_vertex,
+    //         destination_vertex,
+    //         &mut marked,
+    //         &mut edge_to,
+    //     );
+    //
+    //     // If they are connected then from destination_vertex trace the starting_vertex
+    //     if marked[starting_vertex] == marked[destination_vertex] {
+    //         let mut next = destination_vertex;
+    //         let mut path: Vec<T> = Vec::new();
+    //         while next != starting_vertex {
+    //             path.push(self.value(next));
+    //             next = edge_to[next];
+    //         }
+    //         // pushing the destination_vertex
+    //         path.push(self.values[next].clone());
+    //         path.reverse();
+    //         path.into_iter()
+    //     } else {
+    //         Vec::new().into_iter()
+    //     }
+    // }
+    //
+    // fn dfs(&self, v: usize, w: usize, marked: &mut [bool], edge_to: &mut [usize]) {
+    //     marked[v] = true;
+    //     for &adj in self.adjacent_vertex_indices_from_index(v) {
+    //         if !marked[adj] {
+    //             self.dfs(adj, w, marked, edge_to);
+    //             edge_to[adj] = v;
+    //
+    //             // only keep crawling if destination is not found.
+    //             if adj == w {
+    //                 return;
+    //             }
+    //         }
+    //     }
+    // }
+    //
+    // pub fn path_with_bfs(&self, v: &T, w: &T) -> impl Iterator<Item = T> {
+    //     // Making sure v and w exists in the graph
+    //     if self.get_index(v).is_none() || self.get_index(w).is_none() {
+    //         return Vec::new().into_iter();
+    //     }
+    //
+    //     // Finding a path between.
+    //
+    //     let mut edge_to: Vec<usize> = (0..self.count_vertices()).collect();
+    //     let mut marked = vec![false; self.count_vertices()];
+    //     let &starting_vertex = self.get_index(v).unwrap();
+    //     let &destination_vertex = self.get_index(w).unwrap();
+    //     let mut frontier = VecDeque::new();
+    //
+    //     frontier.push_front(starting_vertex);
+    //     marked[starting_vertex] = true;
+    //
+    //     while !frontier.is_empty() {
+    //         if let Some(current_vertex) = frontier.pop_front() {
+    //             for &adj in self.adjacent_vertex_indices_from_index(current_vertex) {
+    //                 if !marked[adj] {
+    //                     frontier.push_front(adj);
+    //                     marked[adj] = true;
+    //                     edge_to[adj] = current_vertex;
+    //
+    //                     if adj == destination_vertex {
+    //                         break;
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     if marked[starting_vertex] == marked[destination_vertex] {
+    //         let mut next = destination_vertex;
+    //         let mut path: Vec<T> = Vec::new();
+    //         while next != starting_vertex {
+    //             path.push(self.value(next));
+    //             next = edge_to[next];
+    //         }
+    //         // pushing the destination_vertex
+    //         path.push(self.values[next].clone());
+    //         path.reverse();
+    //         path.into_iter()
+    //     } else {
+    //         Vec::new().into_iter()
+    //     }
+    //     // If they are connected then from destination_vertex trace the starting_vertex
+    // }
 }
